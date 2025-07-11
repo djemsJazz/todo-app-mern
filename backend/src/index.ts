f005587ae808a00a25ae9582c81243e27e9c81bc
Dahmoun Djamel <dahmdjamel92@gmail.com>
2025-07-10 21:53:26 -0400

    <feat>: setting up DB connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,20 @@
 /* eslint-disable no-console */
 import express, { Express } from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app: Express = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+const dbHost = process.env.DB_CNX_STR;
+
+mongoose.connect(dbHost).then(() => {
+  console.log('DB Connected');
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}).catch((err) => {
+  console.error('DB Connection error', err);
 });
+
