b6fb7000a76c45634c81b94c225bbf0252ad907e
Dahmoun Djamel <dahmdjamel92@gmail.com>
2025-07-11 19:06:26 -0400

    <feat>: adding user controler

---
+++ backend/src/controlers/user.controler.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+
+import User from '../models/user.model';
+import { IUser } from '../types/user';
+
+export const create = async (req: Request, res: Response, next: NextFunction) => {
+  const { body: payload } = req;
+  try {
+    const { email } = payload;
+    const existsUser = await User.findOne({ email });
+    if (existsUser) {
+      res.status(409);
+      throw new Error('User already Exists');
+    }
+    const hash = await bcrypt.hash(payload.password, 10);
+    const newUser: IUser = await new User({ ...payload, password: hash }).save();
+    return res.status(201).send(newUser);
+  } catch (error) {
+    return next(error);
+  }
+};
